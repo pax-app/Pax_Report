cd0f03f28592eee5500c56de3f92239d67877100
Lucas Dutra <ldutra98@gmail.com>
2019-10-01 23:56:57 -0300

    pax-app/Wiki#103 Set database comunication with report service

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from project.api.views import reports_blueprint
+from database import db, migrate
 
 
 # instantiate the app
@@ -11,6 +12,9 @@ def create_app(script_info=None):
     # Set Configuration
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     app.register_blueprint(reports_blueprint)
