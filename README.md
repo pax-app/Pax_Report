1f04f15463297cdf2508739a741430f8bd29bdf0
Lucas Dutra <ldutra98@gmail.com>
2019-10-01 23:58:34 -0300

    pax-app/Wiki#103 Update flask configs for each stage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Pax_Report
+# Report
 Microsserviço responsável por gerenciar o sistema de reportes dos usuários.
+
+## Colocando no ar
+
+Com o Docker e Docker-Compose instalados, basta apenas utilizar os comandos:
+
+```shell
+    sudo docker-compose up
+```
--- project/config.py
+++ project/config.py
@@ -1,19 +1,27 @@
-# services/users/project/config.py
+import os
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
-    """Production configuration"""
-    pass
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class HomologationConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
